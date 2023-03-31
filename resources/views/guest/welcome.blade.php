c375eb32fde51afe977e71f1afc175905bdde3b9
LeonardoContino <leoconti2000@hotmail.it>
2023-03-31 18:25:10 +0200

    navbar doneeeee

--- resources/views/guest/welcome.blade.php
+++ resources/views/guest/welcome.blade.php
@@ -1,31 +1,20 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <!-- Page logo -->
-    <link rel="icon" href="https://cdn.discordapp.com/attachments/1090199171568500760/1091310054222864464/logo_deliverboo_100x250.png" type="image/icon type">
-
     <title>{{ config('app.name', 'Laravel') }}</title>
-
-
     <!-- Fonts -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
     <!-- Usando Vite -->
     @vite(['resources/js/app.js'])
 </head>
-
 <body>
     <div id="home-guest">
-        <div class="container-fluid">
+        <div class="container">
             <header>
                 @include('includes.navbar')
             </header>
@@ -48,5 +37,4 @@
         </div>
     </div>
 </body>
-
 </html>
--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,17 +1,15 @@
-<nav class="navbar navbar-expand-md navbar-light container" id="navbar">
-    <a class=" navbar-brand d-flex align-items-center" href="{{ url('/') }}">
-        <div class="logo mb-1">
-            <img src="https://cdn.discordapp.com/attachments/1090199171568500760/1091310054222864464/logo_deliverboo_100x250.png" class="img" alt="">
-        </div>
-        {{-- config('app.name', 'Laravel') --}}
-    </a>
-
+<nav class="navbar navbar-expand-md navbar-light" id="navbar">
+    <div class="container" >
+        <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
+            <div class="logo mb-1">
+                <img src="https://cdn.discordapp.com/attachments/1090199171568500760/1091310054222864464/logo_deliverboo_100x250.png" class="img" alt="">
+            </div>
+            {{-- config('app.name', 'Laravel') --}}
+        </a>
         <button id="bg-btn" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon btn-color"></span>
         </button>
-
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
             <!-- Right Side Of Navbar -->
             <ul class="navbar-nav ms-auto">
                 <!-- Authentication Links -->
@@ -29,15 +27,20 @@
                     <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                         {{ Auth::user()->name }}
                     </a>
-
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                        @csrf
-                    </form>
-                </div>
-            </li>
-            @endguest
-        </ul>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ url('dashboard') }}">{{__('Dashboard')}}</a>
+                        <a class="dropdown-item" href="{{ url('profile') }}">{{__('Profile')}}</a>
+                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+                @endguest
+            </ul>
+        </div>
     </div>
-
-
 </nav>
