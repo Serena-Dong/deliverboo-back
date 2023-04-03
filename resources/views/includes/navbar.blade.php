9fb0dbe98251cfa546470cbb43c307d8e51b1871
LeonardoContino <leoconti2000@hotmail.it>
2023-04-03 15:41:42 +0200

    add color white

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -24,7 +24,7 @@
                 @endif
                 @else
                 <li class="nav-item dropdown">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    <a id="navbarDropdown " class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                         {{ Auth::user()->name }}
                     </a>
                     <div class="dropdown-menu dropdown-menu-right bg-color-menu" aria-labelledby="navbarDropdown">
