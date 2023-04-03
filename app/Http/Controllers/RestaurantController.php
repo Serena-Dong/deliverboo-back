f2b8c3444f6aee6d83d6da328446c55798fd0c45
Ali-Chouchene <choucheneali9801@gmail.com>
2023-04-03 18:03:45 +0200

    destroy restaurant

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -60,6 +60,10 @@ class RestaurantController extends Controller
      */
     public function destroy(Restaurant $restaurant)
     {
-        //
+
+        // if ($restaurant->image) Storage::delete($restaurant->image);
+        // if (count($restaurant->typologies)) $restaurant->typologies()->detach();
+        $restaurant->delete();
+        return to_route('admin.restaurants.index');
     }
 }
