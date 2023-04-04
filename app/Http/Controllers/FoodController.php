17c1fa1932b72d2ebee8c6e10b9b6c8e5d71ecbb
Antonio-Italiano <antoitalia221@gmail.com>
2023-04-04 12:27:01 +0200

    Added validation

--- app/Http/Controllers/FoodController.php
+++ app/Http/Controllers/FoodController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Food;
+use App\Models\Restaurant;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 
 class FoodController extends Controller
@@ -22,6 +24,14 @@ class FoodController extends Controller
      */
     public function create()
     {
+        $user = Auth::id();
+        $restaurants = Restaurant::where('user_id', $user)->get();
+
+        // SE NON CI SONO RISTORANTI RIMANDA NELL'INDEX
+        if((count($restaurants) == 1)){
+            return to_route('admin.restaurants.index');
+        }
+
         $food = new Food();
         return view('admin.foods.create',compact('food'));
     }
@@ -31,6 +41,24 @@ class FoodController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+
+            'name' => 'required|string|min:5|max:50',
+            'price' => 'required|min:1',
+            'image' => 'nullable|image|mimes:jpeg,jpg,png',
+            'description' => 'nullable|string',
+            'is_public' => 'required|boolean',
+            'restaurant_id' => 'nullable|exists:restaurants,id',
+        ], [
+
+            'name.required' => 'Il Nome è obbligatorio',
+            'name.min' => 'Il Nome deve avere almeno 5 caratteri.',
+            'name.max' => 'Il Nome non deve superare i 50 caratteri.',
+            'image.image' => 'L\'immagine deve essere file di tipo immagine',
+            'image.mimes' => 'Le estensioni accettate sono jpeg, jpg, png',
+            'restaurant_id' => 'Ristorante non valido',
+        ]);
+
         $data = $request->all();
         $food = new Food();
 
@@ -41,7 +69,7 @@ class FoodController extends Controller
 
         $food->fill($data);
         $food->save();
-        return to_route('admin.foods.show', $food->id);
+        return to_route('admin.foods.show', $food->id)->with('type', 'danger')->with('msg', "Il Cibo '$food->name' è stato creato con successo.");
     }
 
     /**
@@ -67,6 +95,24 @@ class FoodController extends Controller
      */
     public function update(Request $request, Food $food)
     {
+        $request->validate([
+
+            'name' => 'required|string|min:5|max:50',
+            'price' => 'required|min:1',
+            'image' => 'nullable|image|mimes:jpeg,jpg,png',
+            'description' => 'nullable|string',
+            'is_public' => 'required|boolean',
+            'restaurant_id' => 'nullable|exists:restaurants,id',
+        ], [
+
+            'name.required' => 'Il Nome è obbligatorio',
+            'name.min' => 'Il Nome deve avere almeno 5 caratteri.',
+            'name.max' => 'Il Nome non deve superare i 50 caratteri.',
+            'image.image' => 'L\'immagine deve essere file di tipo immagine',
+            'image.mimes' => 'Le estensioni accettate sono jpeg, jpg, png',
+            'restaurant_id' => 'Ristorante non valido',
+        ]);
+        
         $data = $request->all();
 
         if(Arr::exists($data, 'image')){
@@ -76,7 +122,7 @@ class FoodController extends Controller
         }
 
         $food->update($data);
-        return view('admin.foods.show', compact('food'));
+        return view('admin.foods.show', compact('food'))->with('type', 'danger')->with('msg', "Il Cibo '$food->name' è stato modificato con successo.");
         
     }
 
@@ -86,5 +132,7 @@ class FoodController extends Controller
     public function destroy(Food $food)
     {
         if($food->image) Storage::delete($food->image);
+        $food->delete();
+        return to_route('admin.foods.index')->with('type', 'danger')->with('msg', "Il Cibo '$food->name' è stato cancellato con successo.");
     }
 }
