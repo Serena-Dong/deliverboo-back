d53a5febdfc194df8cb192c979278173dc35805a
Ali-Chouchene <choucheneali9801@gmail.com>
2023-04-04 11:45:07 +0200

    fixed

--- resources/views/admin/restaurants/index.blade.php
+++ resources/views/admin/restaurants/index.blade.php
@@ -3,35 +3,31 @@
 @section('content')
 <section>
     <div class="container py-5 mt-5">
-        
+
         <div class="text-center my-1 @if(count($restaurants) == 1) d-none @endif">
             <a href="{{ route('admin.restaurants.create') }}" class="btn btn-success">
-            <i class="fas fa-plus me-2"></i> Aggiungi il tuo ristorante
+                <i class="fas fa-plus me-2"></i> Aggiungi il tuo ristorante
             </a>
         </div>
-        
+
         <div class="row d-flex g-5 flex-wrap justify-content-center">
             @foreach ($restaurants as $restaurant)
-                <div class="col-6 my-5 " style="height: 350px; min-width:450px">
-                    <a href="{{ route('admin.restaurants.show', $restaurant['id']) }}" style="text-decoration: none; color:black;">
-                        <div class="card d-flex flex-column-reverse align-items-center h-100 justify-content-between">
-                            <figure class="text-center h-50 w-100 mb-5">
-                                <img src="{{ asset('storage/' . $restaurant->logo) }}" alt="{{$restaurant->name}}" class="img-fluid h-100 rounded-3">
-                            </figure>
-                            <div class="info text-center m-3">
-                                <h1>Ristorante {{$restaurant->name}}</h1>
-                            </div>
+            <div class="col-6 my-5 " style="height: 350px; min-width:450px">
+                <a href="{{ route('admin.restaurants.show', $restaurant['id']) }}" style="text-decoration: none; color:black;">
+                    <div class="card d-flex flex-column-reverse align-items-center h-100 justify-content-between">
+                        <figure class="text-center h-50 w-100 mb-5">
+                            <img src="{{ asset('storage/' . $restaurant->logo) }}" alt="{{$restaurant->name}}" class="img-fluid h-100 rounded-3">
+                        </figure>
+                        <div class="info text-center m-3">
+                            <h1>Ristorante: {{$restaurant->name}}</h1>
                         </div>
-                    </a>
-                    <div class="d-flex justify-content-center align-items-center mt-4 mb-5 gap-3">
-                        <a href="{{ route('admin.restaurants.show', $restaurant->id) }}" class="btn btn-primary text-white"><i class="fa-solid fa-eye"></i> Visualizza</a>
-                        <form action="{{ route('admin.restaurants.destroy', $restaurant->id) }}" method="POST" class="delete-form">
-                            @method('DELETE')
-                            @csrf
-                            <button class="btn btn-danger" type="submit"><i class="fa-solid fa-trash"></i> Elimina</button>
-                        </form>
                     </div>
+                </a>
+                <div class="d-flex justify-content-center align-items-center mt-4 mb-5 gap-3">
+                    <a href="{{ route('admin.restaurants.show', $restaurant->id) }}" class="btn btn-primary text-white"><i class="fa-solid fa-eye"></i> Visualizza</a>
+
                 </div>
+            </div>
             @endforeach
 
         </div>
--- resources/views/admin/restaurants/show.blade.php
+++ resources/views/admin/restaurants/show.blade.php
@@ -1,25 +1,34 @@
-
 @extends('layouts.app')
 @section('content')
 <div class="container d-flex justify-content-center">
     <div class="card mb-3" style="max-width: 80%;">
         <div class="row g-0">
-        <div class="col-md-4 p-3">
-            <img src="{{ asset('storage/' . $restaurant->logo) }}" class="img-fluid rounded-3" alt="{{$restaurant->name}}">
-        </div>
-        <div class="col-md-8">
-            <div class="card-body">
-                <h5 class="card-title">{{$restaurant->name}}</h5>
-                <address class="card-text"> {{$restaurant->address}} </address>
-                <p class="card-text">{{$restaurant->description}}</p>
-                <div class="d-flex d-lg-block justify-content-between">
-                    <p class="card-text"><small class="text-body-secondary">Ordine Minimo: {{$restaurant->min_order}}€</small></p>
-                    <p class="card-text mb-3"><small class="text-body-secondary">Costi di consegna: {{$restaurant->shipment_price}}€</small></p>
-                </div>
-                <a href="{{ route('admin.foods.index')}}">Visualizza Menu</a>
+            <div class="col-md-4 p-3">
+                <img src="{{ asset('storage/' . $restaurant->logo) }}" class="img-fluid rounded-3" alt="{{$restaurant->name}}">
             </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">{{$restaurant->name}}</h5>
+                    <address class="card-text"> {{$restaurant->address}} </address>
+                    <p class="card-text">{{$restaurant->description}}</p>
+                    <div class="d-flex d-lg-block justify-content-between">
+                        <p class="card-text"><small class="text-body-secondary">Ordine Minimo: {{$restaurant->min_order}}€</small></p>
+                        <p class="card-text mb-3"><small class="text-body-secondary">Costi di consegna: {{$restaurant->shipment_price}}€</small></p>
+                    </div>
+                    <a href="{{ route('admin.foods.index')}}"> <i class="fa-solid fa-utensils me-1" style="color: #0561ff;"></i> Visualizza Menu</a>
+                </div>
+            </div>
+
         </div>
+        <div class="d-flex align-items-center justify-content-end me-3 gap-2">
+            <form action="{{ route('admin.restaurants.destroy', $restaurant->id) }}" method="POST" class="delete-form text-center my-3">
+                @method('DELETE')
+                @csrf
+                <button class="btn btn-danger" type="submit"><i class="fa-solid fa-trash"></i> Elimina</button>
+            </form>
+            <a href="{{ route('admin.restaurants.index') }}" class="btn btn-secondary"><i class="fas fa-arrow-left me-1"></i>Indietro</a>
         </div>
-    </div>    
+
+    </div>
 </div>
 @endsection
