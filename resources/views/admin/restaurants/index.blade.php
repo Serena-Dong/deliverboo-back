da12ce00c3129272909f7ba2debe4f4b2043acc0
domenicatonziello <domenica.tonziello10@gmail.com>
2023-04-04 12:53:21 +0200

    padding

--- resources/views/admin/restaurants/index.blade.php
+++ resources/views/admin/restaurants/index.blade.php
@@ -10,9 +10,9 @@
 
         <div class="row p-5 d-flex flex-wrap justify-content-center">
             @foreach ($restaurants as $restaurant)
-            <div class="col-6 pb-4 h-100" style="height: 350px; min-width:450px">
+            <div class="col-6 h-100" style="height: 350px; min-width:450px">
                 <a href="{{ route('admin.restaurants.show', $restaurant['id']) }}" style="text-decoration: none; color:black;">
-                    <div class="card">
+                    <div class="card pb-4">
                         <div class="info text-center py-3">
                             <h1>Ristorante: {{$restaurant->name}}</h1>
                         </div>
