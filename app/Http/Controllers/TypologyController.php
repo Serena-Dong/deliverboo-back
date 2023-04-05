4c473ecf610519ed128d9341f7493602466d7c60
Serena-Dong <serenadong3159@icloud.com>
2023-04-05 12:58:41 +0200

    call api

--- app/Http/Controllers/TypologyController.php
+++ app/Http/Controllers/TypologyController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use App\Models\Typology;
+use App\Models\Restaurant;
 use Illuminate\Http\Request;
 
 class TypologyController extends Controller
@@ -10,9 +12,13 @@ class TypologyController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $user = Auth::id();
+        $restaurant = Restaurant::where('user_id', $user)->get();
+        dd($restaurant);
+        $typology = $restaurant->typologies->toArray();
+        return view('admin.typologies.index', compact('typology'));
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RestaurantController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/restaurants', [RestaurantController::class, 'index']);
+Route::get('/restaurants/{id}', [RestaurantController::class, 'show']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+
     return $request->user();
 });
